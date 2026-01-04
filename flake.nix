a717508ea0681ceb1bf73e3640a3dd09e3d32903
Linus Dikomey <l.dikomey03@gmail.com>
2026-01-04 17:23:27 +0100

    bump: llvm 21, bump+format flake

--- flake.nix
+++ flake.nix
@@ -8,62 +8,80 @@
       };
     };
   };
-  outputs = {
-    self,
-    nixpkgs,
-    rust-overlay,
-  }: let
-    overlays = [(import rust-overlay)];
-    supportedSystems = ["aarch64-darwin" "aarch64-linux" "x86_64-darwin" "x86_64-linux"];
-    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-    pkgsFor = system: import nixpkgs {inherit system overlays;};
-    rustToolchain = pkgs: pkgs.rust-bin.stable.latest.default;
-    rustPlatform = pkgs:
-      pkgs.makeRustPlatform {
-        cargo = rustToolchain pkgs;
-        rustc = rustToolchain pkgs;
-      };
-  in {
-    packages = forAllSystems (
-      system: let
-        pkgs = pkgsFor system;
-      in {
-        default = (rustPlatform pkgs).buildRustPackage {
-          pname = "eye";
-          version = "0.1";
-          cargoLock = {
-            lockFile = ./Cargo.lock;
-            outputHashes = {
-              "color-format-0.1.0" = "sha256-ErorDyhDtycDgDb8tyQe6TQ+lz552azJ4G1V5bhOb/E=";
-            };
-          };
-          buildFeatures = ["lsp"];
-          src = pkgs.lib.cleanSource ./.;
-          nativeBuildInputs = with pkgs; [
-            llvmPackages_19.llvm
-            libxml2
-          ];
-          env.EYE_DEFAULT_STD = "${placeholder "out"}/lib/std";
-          postInstall = ''
-            mkdir -p $out/lib
-            cp -r std $out/lib
-          '';
+  outputs =
+    {
+      self,
+      nixpkgs,
+      rust-overlay,
+    }:
+    let
+      overlays = [ (import rust-overlay) ];
+      supportedSystems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      pkgsFor = system: import nixpkgs { inherit system overlays; };
+      rustToolchain = pkgs: pkgs.rust-bin.stable.latest.default;
+      rustPlatform =
+        pkgs:
+        pkgs.makeRustPlatform {
+          cargo = rustToolchain pkgs;
+          rustc = rustToolchain pkgs;
         };
-      }
-    );
-    devShells = forAllSystems (system: let
-      pkgs = pkgsFor system;
-    in {
-      default = pkgs.mkShell {
-        inputsFrom = [self.packages.${system}.default];
-        nativeBuildInputs = [
-          ((rustToolchain pkgs).override {
-            extensions = ["rust-analyzer" "rust-src"];
-          })
-          pkgs.python3
-        ];
-        RUST_BACKTRACE = 1;
-      };
-    });
-  };
+    in
+    {
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = pkgsFor system;
+        in
+        {
+          default = (rustPlatform pkgs).buildRustPackage {
+            pname = "eye";
+            version = "0.1";
+            cargoLock = {
+              lockFile = ./Cargo.lock;
+              outputHashes = {
+                "color-format-0.1.0" = "sha256-ErorDyhDtycDgDb8tyQe6TQ+lz552azJ4G1V5bhOb/E=";
+              };
+            };
+            buildFeatures = [ "lsp" ];
+            src = pkgs.lib.cleanSource ./.;
+            nativeBuildInputs = with pkgs; [
+              llvmPackages_21.llvm
+              libxml2
+            ];
+            env.EYE_DEFAULT_STD = "${placeholder "out"}/lib/std";
+            postInstall = ''
+              mkdir -p $out/lib
+              cp -r std $out/lib
+            '';
+          };
+        }
+      );
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = pkgsFor system;
+        in
+        {
+          default = pkgs.mkShell {
+            inputsFrom = [ self.packages.${system}.default ];
+            nativeBuildInputs = [
+              ((rustToolchain pkgs).override {
+                extensions = [
+                  "rust-analyzer"
+                  "rust-src"
+                ];
+              })
+              pkgs.python3
+            ];
+            RUST_BACKTRACE = 1;
+          };
+        }
+      );
+    };
 }
